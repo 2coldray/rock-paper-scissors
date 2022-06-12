7b1626ee6e83f0d5d3d7ac5382be9f8bdad5365b
Rayshawn <rayshawnbray@gmail.com>
2022-06-12 11:33:05 -0400

    Create computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('Hello World');
+
+// Computer Play Function
+
+// create rock paper scissors array
+const computerChoices = ['rock', 'paper', 'scissors'];
+
+function computerPlay() {
+    // return a random instance of rock of an element in the above array
+    console.log(computerChoices[Math.floor(Math.random() * computerChoices.length)]);
+    return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
+
+computerPlay();
