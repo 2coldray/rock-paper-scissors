1eb4c773069675f74629474b4bebea2e1ea83c1e
Rayshawn <rayshawnbray@gmail.com>
2022-06-13 11:51:22 -0400

    Add event listeners to buttons and test to make sure it works

--- script.js
+++ script.js
@@ -18,6 +18,42 @@ function computerPlay() {
 
 //const playerSelection = 'rock';
 //const computerSelection = computerPlay();
+
+// bring in button elements
+const rockBtn = document.getElementById('rock');
+const paperBtn = document.getElementById('paper');
+const scissorsBtn = document.getElementById('scissors');
+
+// add event listener to rock button
+rockBtn.addEventListener('click', function() {
+    // get the text of the rock
+    const rockText = rockBtn.textContent;
+    // get the computer selection
+    const computerSelection = computerPlay();
+    // play round
+    playRound(rockText, computerSelection);
+});
+
+// add event listener to the paper button
+paperBtn.addEventListener('click', function() {
+    // get text of paper button
+    const paperText = paperBtn.textContent;
+    // get the computer selection
+    const computerSelection = computerPlay();
+    // play round
+    playRound(paperText, computerSelection);
+});
+
+// add event listener to scissors button
+scissorsBtn.addEventListener('click', function() {
+    // get text of scissors button
+    const scissorsText = scissorsBtn.textContent;
+    // get computer selection
+    const computerSelection = computerPlay();
+    //playRound
+    playRound(scissorsText, computerSelection);
+})
+
 let userScore = 0;
 let computerScore = 0;
 let ties = 0;
@@ -53,24 +89,24 @@ function playRound(playerSelection, computerSelection) {
 }
 
 //playRound(playerSelection, computerSelection);
-// let play = true;
+let play = true;
 // Game function
 // write variables to keep score
-// function game() {
-//     // loop to 5 and call play around on each iteration
-//     for (let i = 0; i < 5; i++) {
-//         const playerSelection = prompt('Enter rock, paper, or scissors');
-//         const computerSelection = computerPlay();
-//         playRound(playerSelection, computerSelection);
-//     }
-//     play = false;
-//     if (userScore > computerScore) {
-//         console.log('You won beat the computer');
-//     } else {
-//         console.log('You lost to the computer');
-//     }
+function game() {
+    // loop to 5 and call play around on each iteration
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Enter rock, paper, or scissors');
+        const computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+    }
+    play = false;
+    if (userScore > computerScore) {
+        console.log('You won beat the computer');
+    } else {
+        console.log('You lost to the computer');
+    }
 
-// }
+}
 
 // game loop created
 // while (play) {
